41a0a8a294aeb1fee40bac891304b998266e5cd8
denysmarrow <mzmarrowz@gmail.com>
2022-01-04 18:57:43 -0500

    adding notes

---
+++ Notes/DataTypes/Main.java
@@ -0,0 +1,10 @@
+package com.careerdevs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myAge = 30;
+        int herAge = myAge;
+        System.out.println(herAge);
+    }
+}
