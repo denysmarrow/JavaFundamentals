5d59f3488a11f4f5f4cb268b5944365a1a377518
denysmarrow <mzmarrowz@gmail.com>
2022-01-06 16:34:43 -0500

    adding notes

--- src/com/careerdevs/intro/dataTypes.java
+++ src/com/careerdevs/intro/dataTypes.java
@@ -2,6 +2,11 @@ package com.careerdevs.intro;
 
 public class dataTypes {
     public static void main(String[] args) {
-
+        byte myAge = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+        System.out.println(herAge);
     }
 }
