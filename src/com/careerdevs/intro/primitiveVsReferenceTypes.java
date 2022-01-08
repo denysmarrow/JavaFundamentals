49f5084bc1b512e464cef294eb790e4fa36145c4
denysmarrow <mzmarrowz@gmail.com>
2022-01-07 22:24:07 -0500

    new commit

--- src/com/careerdevs/intro/primitiveVsReferenceTypes.java
+++ src/com/careerdevs/intro/primitiveVsReferenceTypes.java
@@ -1,11 +1,14 @@
 package com.careerdevs.intro;
 
+import java.awt.*;
+
 public class primitiveVsReferenceTypes {
     public static void main(String[] args) {
-        byte x = 1;
-        byte y = x;
-        x = 2;
-        System.out.println(y);
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        // referencing the same object
+        point1.x = 2;
+        System.out.println(point2);
 
     }
 }
