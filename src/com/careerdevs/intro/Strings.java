da3153de72595985ea9aecd71b7627e14aba1629
denysmarrow <mzmarrowz@gmail.com>
2022-01-07 22:51:26 -0500

    adding notes

--- src/com/careerdevs/intro/Strings.java
+++ src/com/careerdevs/intro/Strings.java
@@ -1,9 +1,12 @@
 package com.careerdevs.intro;
 
+import java.util.Locale;
+
 public class Strings {
     public static void main(String[] args) {
-        String message = "Hello World" + "!!";
+        String message = "  Hello World" + "!!  ";
 
-        System.out.println(message.replace("!", "*"));
+        System.out.println(message.trim());
+        System.out.println(message);
     }
 }
