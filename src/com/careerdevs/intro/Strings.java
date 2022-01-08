d6613fca4426121fc4c3b7846f6c33214a71e3c1
denysmarrow <mzmarrowz@gmail.com>
2022-01-07 22:46:24 -0500

    adding notes

---
+++ src/com/careerdevs/intro/Strings.java
@@ -0,0 +1,9 @@
+package com.careerdevs.intro;
+
+public class Strings {
+    public static void main(String[] args) {
+        String message = "Hello World" + "!!";
+
+        System.out.println(message.replace("!", "*"));
+    }
+}
