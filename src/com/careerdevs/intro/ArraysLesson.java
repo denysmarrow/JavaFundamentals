d960454ad44b12f8f0e84e170c354168178a3a34
denysmarrow <mzmarrowz@gmail.com>
2022-01-09 00:00:31 -0500

    adding notes

---
+++ src/com/careerdevs/intro/ArraysLesson.java
@@ -0,0 +1,13 @@
+package com.careerdevs.intro;
+
+
+import java.util.Arrays;
+
+public class ArraysLesson {
+    public static void main(String[] args) {
+        int[] numbers = {2, 3, 5, 1, 4};
+        Arrays.sort(numbers);
+
+        System.out.println(Arrays.toString(numbers));
+    }
+}
