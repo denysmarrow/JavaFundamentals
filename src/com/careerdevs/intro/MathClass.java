de2dcae8b5fdf5632b9092fffd8b53b5caf9e444
denysmarrow <mzmarrowz@gmail.com>
2022-01-10 15:54:12 -0500

    new commit

---
+++ src/com/careerdevs/intro/MathClass.java
@@ -0,0 +1,22 @@
+package com.careerdevs.intro;
+
+public class MathClass {
+    public static void main(String[] args) {
+        int result = (int) (Math.random() * 100);
+        System.out.println(result);
+
+    }
+}
+
+// .ciel Rounds to the next largest whole number
+// .floor rounds largest integer that is small or equal to said number
+// .random() * 100 --> gives any random number between 0-100
+
+
+//public class MathClass {
+//    public static void main(String[] args) {
+//        int result = (int) Math.round(Math.random() * 100);
+//        System.out.println(result);
+//
+//    }
+//}
