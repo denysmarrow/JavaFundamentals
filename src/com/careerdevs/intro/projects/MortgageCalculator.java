f09d497cea4508d196b741d4e8f5076f55a6f459
denysmarrow <mzmarrowz@gmail.com>
2022-01-10 18:26:58 -0500

    new commit

--- src/com/careerdevs/intro/projects/MortgageCalculator.java
+++ src/com/careerdevs/intro/projects/MortgageCalculator.java
@@ -1,7 +1,17 @@
 package com.careerdevs.intro.projects;
 
+import java.util.Scanner;
+
 public class MortgageCalculator {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principal: ");
+
+        System.out.print("Annual Interest Rate: ");
+
+        int principal = scanner.nextInt();
+        int annual = scanner.nextInt();
+
 
     }
 }
