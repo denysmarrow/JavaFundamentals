8903c7d66ca57dc382c6a9a26cf160eb26652bea
denysmarrow <mzmarrowz@gmail.com>
2022-01-10 16:52:12 -0500

    new commit

---
+++ src/com/careerdevs/intro/FormattingNumbers.java
@@ -0,0 +1,21 @@
+package com.careerdevs.intro;
+
+import java.text.NumberFormat;
+
+public class FormattingNumbers {
+    public static void main(String[] args) {
+//        NumberFormat.getPercentInstance().format(0.1); //method chaining
+        String result = NumberFormat.getPercentInstance().format(0.1);
+        System.out.println(result);
+
+    }
+}
+
+//public class FormattingNumbers {
+//    public static void main(String[] args) {
+//        NumberFormat percent = NumberFormat.getPercentInstance();
+//        String result = percent.format(0.1);
+//        System.out.println(result);
+//
+//    }
+//}
