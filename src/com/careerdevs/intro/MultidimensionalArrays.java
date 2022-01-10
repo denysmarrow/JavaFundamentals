3faf23cbb46fad63cd1c2cb9c8a7cc4a5301abb2
denysmarrow <mzmarrowz@gmail.com>
2022-01-10 15:13:57 -0500

    new commit

---
+++ src/com/careerdevs/intro/MultidimensionalArrays.java
@@ -0,0 +1,10 @@
+package com.careerdevs.intro;
+
+import java.util.Arrays;
+
+public class MultidimensionalArrays {
+    public static void main(String[] args) {
+        int[][] numbers = {{1,2,3,}, {4,5,6}};
+        System.out.println(Arrays.deepToString(numbers));
+    }
+}
