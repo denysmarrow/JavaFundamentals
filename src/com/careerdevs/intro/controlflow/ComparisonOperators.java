b669521639c1b122965e0e78bfab6b1e04cd8d47
denysmarrow <mzmarrowz@gmail.com>
2022-01-13 18:47:46 -0500

    new commit

---
+++ src/com/careerdevs/intro/controlflow/ComparisonOperators.java
@@ -0,0 +1,10 @@
+package com.careerdevs.intro.controlflow;
+
+public class ComparisonOperators {
+    public static void main(String[] args) {
+        int x = 1;
+        int y =1;
+        System.out.println(x != y);//boolean expression
+    }
+}
+
