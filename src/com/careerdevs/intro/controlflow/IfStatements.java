0ff152d2bae6ba57ffa28182f2d96f0f2ed16a7d
denysmarrow <mzmarrowz@gmail.com>
2022-01-13 19:00:16 -0500

    new commit

---
+++ src/com/careerdevs/intro/controlflow/IfStatements.java
@@ -0,0 +1,17 @@
+package com.careerdevs.intro.controlflow;
+
+public class IfStatements {
+    public static void main(String[] args) {
+        int temp = 25;
+
+        if (temp > 30) {
+            System.out.println("It's a hot day");
+            System.out.println("Drink water");
+        }
+        else if (temp > 20)
+            System.out.println("Beautiful day");
+        else
+            System.out.println("Cold day");
+
+    }
+}
--- src/com/careerdevs/intro/controlflow/LogicalOperators.java
+++ src/com/careerdevs/intro/controlflow/LogicalOperators.java
@@ -1,4 +1,14 @@
 package com.careerdevs.intro.controlflow;
 
 public class LogicalOperators {
+    public static void main(String[] args) {
+        boolean hasHighIncome = true;
+        boolean hasGoodCredit = true;
+        boolean hasCriminalRecord = false;
+        boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
+        System.out.println();
+    }
 }
+
+//&& = and operator
+// || = or operator
