3680fc54527308aa799af953a1171988d7d196e4
denysmarrow <mzmarrowz@gmail.com>
2022-01-27 18:41:17 -0500

    for loops

---
+++ src/com/careerdevs/intro/controlflow/ForLoops.java
@@ -0,0 +1,10 @@
+package com.careerdevs.intro.controlflow;
+
+public class ForLoops {
+    public static void main(String[] args) {
+        for (int i = 5; i > 0; i--) {
+            System.out.println("Hello World " + i);
+
+        }
+    }
+}
