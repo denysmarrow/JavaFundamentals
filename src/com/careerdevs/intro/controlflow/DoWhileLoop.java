3a661610fd9ac2256b8c9e054ddabaac807ab635
denysmarrow <mzmarrowz@gmail.com>
2022-02-09 21:01:22 -0500

    break and continue

--- src/com/careerdevs/intro/controlflow/DoWhileLoop.java
+++ src/com/careerdevs/intro/controlflow/DoWhileLoop.java
@@ -6,17 +6,16 @@ public class DoWhileLoop {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = "";
-        while (!input.equals("quit")) {
+        while (true) {
             System.out.println("Input: ");
             input = scanner.next().toLowerCase();
-            System.out.println(input);
+            if (input.equals("pass"))
+                continue;
+            if (input.equals("quit"))
+                break;
+                System.out.println(input);
 
     }
-      do {
-          System.out.println("Input: ");
-          input = scanner.next().toLowerCase();
-          System.out.println(input);
-         }
-      while (!input.equals("quit")) ;
+
     }
 }
